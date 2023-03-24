9e4b1d0ce51ce2253d7608fb74000c49795a4c7a
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-03-24 17:09:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-api_id = int(os.environ.get("API_ID", "26662728"))
-api_hash = os.environ.get("API_HASH", "562302fa6d2e82bf399880f3b1250cf8")
-bot_token = os.environ.get("BOT_TOKEN", "5835706516:AAETpXkhqCVc1isTRRHc83r8fe9nbXVcffo")
+api_id = int(os.environ.get("API_ID", "12857763"))
+api_hash = os.environ.get("API_HASH", "7b71e8bca0d5e1c6d8383ae818d9ec8d")
+bot_token = os.environ.get("BOT_TOKEN", "5818056238:AAGpnZ2X5RleSv5b8NBQSAZyAnUGc_iAp4Y")
 # =========================================================== #
 
-db_url = os.environ.get("DB_URL", "mongodb+srv://p:Pendii11@cluster0.m8kujry.mongodb.net/?retryWrites=true&w=majority")
-db_name = os.environ.get("DB_NAME", "menfess")
+db_url = os.environ.get("DB_URL", "mongodb+srv://fadhilubot:ffZiodLdn2mxVIUp@cluster0.0pba0ap.mongodb.net/?retryWrites=true&w=majority")
+db_name = os.environ.get("DB_NAME", "fadhilubot")
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "-1001277245439"))
-channel_2 = int(os.environ.get("CHANNEL_2", "-1001511422552"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1001307462253"))
+channel_1 = int(os.environ.get("CHANNEL_1", "-1001809708760"))
+channel_2 = int(os.environ.get("CHANNEL_2", "-1001537697834"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1001554653997"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "1983980399"))
+id_admin = int(os.environ.get("ID_ADMIN", "1345594412"))
 # =========================================================== #
 
-batas_kirim = int(os.environ.get("BATAS_KIRIM", "5"))
-batas_talent = int(os.environ.get("BATAS_TALENT", "10"))
+batas_kirim = int(os.environ.get("BATAS_KIRIM", "1"))
+batas_talent = int(os.environ.get("BATAS_TALENT", "15"))
 batas_daddy_sugar = int(os.environ.get("BATAS_DADDY_SUGAR", "10"))
 batas_moansgirl = int(os.environ.get("BATAS_MOANSGIRL", "10"))
 batas_moansboy = int(os.environ.get("BATAS_MOANSBOY", "10"))
@@ -35,22 +35,22 @@ biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "40"))
 biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "30"))
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#AlterGirl #AlterBoy #AlterAsk #AlterFind #AlterSpill #AlterStory").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#FbsBoy #FbsGirl #FbsAsk #FbsFind #FbsSpill #FbsStory").replace(" ", "|").lower()
 # =========================================================== #
 
-pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph/file/a6a2175f88089079b77ce.jpg")
-pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph/file/932c903b1dcfb8f9780e6.jpg")
+pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph//file/0d5a174c3dd2a156d02e8.jpg")
+pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph//file/a26cef38c65c132d555a5.jpg")
 # =========================================================== #
 
-pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
-start_msg = os.environ.get("START_MSG", "Hai {fullname} 🌱\n\nIni adalah bot menfess, semua pesan yang kamu kirim akan masuk ke channel secara anonymous. ketik /help")
+pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat Mengirim, Harap Join Terllebih Dahulu Untuk Mengirim Menfess ya Efbees")
+start_msg = os.environ.get("START_MSG", "Hai {fullname} 🌱\n\nIni adalah bot menfess ya Efbees, semua pesan yang kamu kirim akan masuk ke channel secara anonymous. ketik /help")
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
 {mention}, pesan mu gagal terkirim silahkan gunakan hastag:
 
-#AlterBoy / #AlterGirl untuk Mencari Pasangan, Teman , Partner dll
-#AlterAsk untuk Bertanya
-#AlterStory untuk Berbagi Cerita
-#AlterSpill untuk Spill Masalah
-#AlterFind untuk Mencari Pasangan, Teman, Partner dll
+#FbsBoy / #FbsGirl untuk Mencari Pasangan, Teman , Partner dll
+#FbsAsk untuk Bertanya
+#FbsStory untuk Berbagi Cerita
+#FbsSpill untuk Spill Masalah
+#FbsFind untuk Mencari Pasangan, Teman, Partner dll
 """)
