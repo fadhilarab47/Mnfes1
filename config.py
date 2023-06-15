378ffbd2cda8c738926243a7552711240a706ad7
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-06-16 01:29:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ db_name = os.environ.get("DB_NAME", "Menfes2")
 
 channel_1 = int(os.environ.get("CHANNEL_1", "-1001976239415"))
 channel_2 = int(os.environ.get("CHANNEL_2", "-1001500631464"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1001684568256"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1001774113558"))
 # =========================================================== #
 
 id_admin = int(os.environ.get("ID_ADMIN", "1998691839"))
