3eb0395eca1c7d550300ce1ce1e7e969a2f138a4
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-06-28 12:37:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ batas_gfrent = int(os.environ.get("BATAS_GFRENT", "10"))
 batas_bfrent = int(os.environ.get("BATAS_BFRENT", "10"))
 # =========================================================== #
 
-biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "100"))
+biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "50"))
 biaya_talent = int(os.environ.get("BIAYA_TALENT", "500"))
 biaya_daddy_sugar = int(os.environ.get("BIAYA_DADDY_SUGAR", "500"))
 biaya_moansgirl = int(os.environ.get("BIAYA_MOANSGIRL", "500"))
