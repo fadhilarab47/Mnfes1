d06a37f180e0f39887432587ea17d159fd6cdb3d
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-21 15:55:31 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -55,7 +55,6 @@ class Helper():
     async def pesan_langganan(self):
         link_1 = await self.bot.export_chat_invite_link(config.channel_1)
         link_2 = await self.bot.export_chat_invite_link(config.channel_2)
-        link_2 = await self.bot.export_chat_invite_link(config.channel_3)
         markup = InlineKeyboardMarkup([
             [InlineKeyboardButton('Group Base I', url=link_3)]
             [InlineKeyboardButton('Channel base', url=link_1), InlineKeyboardButton('Group base II', url=link_2)],
